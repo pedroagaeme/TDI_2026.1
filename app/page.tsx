14c7f2705b3aea125d3bd837321e3a1bff7f4197
pedroagaeme <futurograduado@gmail.com>
2026-04-29 13:19:16 -0300

    Fix import alias in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { VideoQuizApp } from '@/TDI_2026.1/components/video-quiz-app';
+import { VideoQuizApp } from '@/components/video-quiz-app';
 
 export default function Page() {
   return <VideoQuizApp />;
